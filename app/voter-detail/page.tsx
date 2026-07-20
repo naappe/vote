584eddf0c1afe38bf6cccd3205a624c8fe82d8e3
naappe <naappe@gmail.com>
2026-07-20 21:35:58 +0500

    Add voter detail route

---
+++ app/voter-detail/page.tsx
@@ -0,0 +1,3 @@
+import AppLayout from '../../layout/AppLayout';
+import VoterDetailContent from '../../components/VoterDetailContent';
+export default function Page(){return <AppLayout><VoterDetailContent/></AppLayout>}
