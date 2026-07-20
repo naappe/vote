0e8762acaf60a2b3403ad315c32dd8c31c915f8d
naappe <naappe@gmail.com>
2026-07-20 21:36:06 +0500

    Add call center route

---
+++ app/call-center/page.tsx
@@ -0,0 +1,3 @@
+import AppLayout from '../../layout/AppLayout';
+import OperationsListContent from '../../components/OperationsListContent';
+export default function Page(){return <AppLayout><OperationsListContent mode="calls"/></AppLayout>}
