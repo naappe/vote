cf07ff4d67f3a52856b3a344eb64a541dd22a529
naappe <naappe@gmail.com>
2026-07-21 12:59:30 +0500

    Remove campaign showcase from assignments page

--- app/assignments/page.tsx
+++ app/assignments/page.tsx
@@ -1,3 +1,3 @@
 import AppLayout from '../../layout/AppLayout';
 import AssignmentsContent from '../../components/AssignmentsContent';
-export default function Page(){return <AppLayout><AssignmentsContent/></AppLayout>}
+export default function Page(){return <AppLayout showCampaignShowcase={false}><AssignmentsContent/></AppLayout>}
