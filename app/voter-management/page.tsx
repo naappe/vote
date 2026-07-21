f8be81d81b15460ee1baf61798487144a28c2aa5
naappe <naappe@gmail.com>
2026-07-21 11:05:42 +0500

    Remove legacy voter management route

--- app/voter-management/page.tsx
+++
@@ -1,3 +0,0 @@
-import AppLayout from '../../layout/AppLayout';
-import VoterManagementContent from '../../components/VoterManagementContent';
-export default function Page(){return <AppLayout><VoterManagementContent/></AppLayout>}
