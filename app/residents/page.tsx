1698888db7fa30576d0c8dcd720e93f77c13f3b4
naappe <naappe@gmail.com>
2026-07-21 10:58:34 +0500

    Add final residents route

---
+++ app/residents/page.tsx
@@ -0,0 +1,3 @@
+import AppLayout from '../../layout/AppLayout';
+import VoterManagementContent from '../../components/VoterManagementContent';
+export default function Page(){return <AppLayout><VoterManagementContent/></AppLayout>}
