a6c74b86b0d3d6557528f2d5b10dfa410aefd78e
naappe <naappe@gmail.com>
2026-07-21 11:06:44 +0500

    Delete old verification route

--- app/admin-verification/page.tsx
+++
@@ -1,3 +0,0 @@
-import AppLayout from '../../layout/AppLayout';
-import AdminVerificationContent from '../../components/AdminVerificationContent';
-export default function Page(){return <AppLayout><AdminVerificationContent/></AppLayout>}
